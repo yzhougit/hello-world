52f1f92302c152d23a19e737b2aea085fcd714c4
yzhougit <72467406+yzhougit@users.noreply.github.com>
2020-10-06 16:31:18 -0400

    Add files via upload

---
+++ devtest.config.js
@@ -0,0 +1,14 @@
+// NOTE object below must be a valid JSON
+window.DevTest = $.extend(true, window.DevTest, {
+    "config": {
+        "layoutSet": "navbar",
+        "animationSet": "default",
+        "navigation": [
+            {
+                "title": "About",
+                "onExecute": "#About",
+                "icon": "info"
+            }
+        ]
+    }
+});
